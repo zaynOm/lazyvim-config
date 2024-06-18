b6c43087bc1c29e2e2a875a5ba8781b08ac5e159
zaynom <ouaziz0626@gmail.com>
2024-06-18 19:47:09 +0100

    disable pyflakes, flake8, mccabe

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,9 +7,13 @@ return {
         settings = {
           pylsp = {
             plugins = {
-              rope_autoimport = {
-                enabled = true,
-              },
+              rope_autoimport = { enabled = true },
+              pylsp_mypy = { enabled = true },
+              flake8 = { enabled = false },
+              pyflakes = { enabled = false },
+              mccabe = { enabled = false },
+              -- desabled by default
+              pylint = { enabled = false },
             },
           },
         },
