74b34a017d5263a6e049aaab7cede71ccd627464
zaynom <ouaziz0626@gmail.com>
2024-07-17 13:05:30 +0100

    change neo-tree position to right

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -4,4 +4,9 @@ return {
     { "<leader>e", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
     { "<leader>E", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
   },
+  opts = {
+    window = {
+      position = "right",
+    },
+  },
 }
