78743a4e6e6b73afd1b3b10dfecb649d03274e5b
zaynom <ouaziz0626@gmail.com>
2024-10-18 18:05:23 +0100

    Fix lualine not showing commands like 'recording keys'

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,32 +1,40 @@
 return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
-  opts = function()
-    return {
-      options = {
-        sections = {
-          lualine_a = {
-            file = 1,
-          },
+  opts = {
+    options = {
+      section_separators = { left = "", right = "" },
+      component_separators = { left = "|", right = "|" },
+    },
+    sections = {
+      lualine_b = { "branch", "diff" },
+      lualine_x = {
+        -- stylua: ignore
+        {
+          function() return require("noice").api.status.command.get() end,
+          cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+          color = function() return LazyVim.ui.fg("Statement") end,
         },
-        section_separators = { left = "", right = "" },
-        component_separators = { left = "", right = "" },
-        -- disabled_filetypes = { "NvimTree" }, -- Add this line
+        -- stylua: ignore
+        {
+          function() return require("noice").api.status.mode.get() end,
+          cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+          color = function() return LazyVim.ui.fg("Constant") end,
+        },
+        -- stylua: ignore
+        {
+          function() return "  " .. require("dap").status() end,
+          cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+          color = function() return LazyVim.ui.fg("Debug") end,
+        },
+        -- stylua: ignore
+        {
+          require("lazy.status").updates,
+          cond = require("lazy.status").has_updates,
+          color = function() return LazyVim.ui.fg("Special") end,
+        },
       },
-      -- extensions = {
-      --   "nvim-tree", -- Use the built-in extension for NvimTree
-      --   {
-      --     sections = {
-      --       lualine_a = {},
-      --       lualine_b = {},
-      --       lualine_c = { "NvimTree" },
-      --       lualine_x = {},
-      --       lualine_y = {},
-      --       lualine_z = {},
-      --     },
-      --     filetypes = { "NvimTree" },
-      --   },
-      -- },
-    }
-  end,
+      lualine_z = {},
+    },
+  },
 }
