3c65cc57685f8efc87c6fbf2d1626aa0c8252164
zaynom <ouaziz0626@gmail.com>
2024-11-20 23:49:38 +0100

    Add new file creation with name keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,3 +15,14 @@ vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", {})
 -- theprimagen maps
 vim.keymap.set("v", "<leader>p", [["_dP]])
 vim.keymap.set({ "n", "v" }, "<leader>h", [["_d]])
+
+-- remap new file keymaps
+-- vim.keymap.set('n', '<leader>fn', '', opts?)
+vim.keymap.set("n", "<leader>n", function()
+  local file_name = vim.fn.input("Enter file name: ")
+  if file_name and file_name ~= "" then
+    vim.cmd("edit " .. file_name) -- Create and open the file
+  else
+    print("No file name provided!")
+  end
+end, { desc = "Create new file with name" })
