f41971c02e948165a88cff27d1debdc77e1a2a15
zaynom <ouaziz0626@gmail.com>
2024-12-06 20:49:34 +0100

    Add named files creation

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- vim.api.nvim_create_autocmd("BufNewFile", {
+--   print("This is cool"),
+--   callback = function()
+--     local file_name = vim.fn.input("Enter file name: ")
+--     if file_name and file_name ~= "" then
+--       vim.cmd("new " .. file_name)
+--     end
+--   end,
+-- })
